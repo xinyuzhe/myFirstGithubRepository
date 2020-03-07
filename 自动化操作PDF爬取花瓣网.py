568bea42e6db55838a8366a5d7939560c5003eec
XinYuZhe <61902024+XinYuZhe@users.noreply.github.com>
2020-03-07 17:22:45 +0800

    删除头注释信息
    第一次在Github编辑文件！

--- 自动化操作PDF爬取花瓣网.py
+++ 自动化操作PDF爬取花瓣网.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Author: Anderson
-# @Date:   2019-11-14 16:30:55
-# @Last Modified by:   Anderson
-# @Last Modified time: 2019-12-30 15:32:39
 from makerbean import web_crawler_bot as wbot
 from makerbean import excel_bot as ebot
 from makerbean import data_analysis_bot as dbot
